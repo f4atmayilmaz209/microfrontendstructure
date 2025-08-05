60661c8dd655ea72704b2fd8efa48f2f02877324
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2025-08-05 09:22:01 +0300

    fixed docker url

--- products/next.config.js
+++ products/next.config.js
@@ -23,7 +23,7 @@ module.exports = {
           './Products': './components/Products.tsx',
         },
         remotes: {
-          host: 'host@http://ip172-18-0-25-d28inlc69qi000aie8bg-3000.direct.labs.play-with-docker.com/_next/static/chunks/remoteEntry.js',
+          host: 'host@http://ip172-18-0-12-d28pp6469qi000fe4e10-3000.direct.labs.play-with-docker.com/_next/static/chunks/remoteEntry.js',
         },
         shared: {
           react: { singleton: true, requiredVersion: "^18.2.0", eager: false },
